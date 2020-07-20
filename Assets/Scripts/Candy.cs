4780acca7ee492d8ee7de3fa5ddc9bd561a7cfe4
Stephano Apiolaza <stephanoapiolaza@gmail.com>
2020-07-19 23:12:07 -0400

    chore(app): setup bootstraping

---
+++ Assets/Scripts/Candy.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candy : MonoBehaviour
+{
+
+    private static Color selectedColor = new Color(0.5f, 0.5f, 0.5f, 1.0f);
+    private static Candy previousSelected = null;
+    private SpriteRenderer spriteRenderer;
+    private bool isSelected = false;
+    public int id;
+
+    private Vector2[] adjacentDirections = new Vector2[]
+    {
+        Vector2.up,
+        Vector2.down,
+        Vector2.left,
+        Vector2.right
+    };
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void SelectCandy()
+    {
+        this.isSelected = true;
+        spriteRenderer.color = selectedColor;
+        previousSelected = gameObject.GetComponent<Candy>();
+    }
+
+    private void DeselectCandy()
+    {
+        this.isSelected = false;
+        spriteRenderer.color = Color.white;
+        previousSelected = null;
+    }
+
+    private void OnMouseDown()
+    {
+        if(spriteRenderer.sprite == null || BoardManager.sharedInstance.isShifting)
+        {
+            return;
+        }
+        if(this.isSelected)
+        {
+            this.DeselectCandy();
+        }
+        else
+        {
+            if (previousSelected == null)
+            {
+                this.SelectCandy();
+            }
+            else
+            {
+                if(this.CanSwipe())
+                {
+                    SwapSprite(previousSelected);
+                    previousSelected.FindallMatches();
+                    previousSelected.DeselectCandy();
+                    this.FindallMatches();
+                    GUIManager.sharedInstance.MoveCounter--;
+                }
+                else
+                {
+                    previousSelected.DeselectCandy();
+                    SelectCandy();
+                }
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SwapSprite(Candy newCandy)
+    {
+        if(spriteRenderer.sprite == newCandy.GetComponent<SpriteRenderer>().sprite)
+        {
+            return;
+        }
+        Sprite oldCandy = newCandy.spriteRenderer.sprite;
+        newCandy.spriteRenderer.sprite = this.spriteRenderer.sprite;
+        this.spriteRenderer.sprite = oldCandy;
+
+        int tempId = newCandy.id;
+        newCandy.id = this.id;
+        this.id = tempId;
+    }
+
+    private GameObject GetNeighbor(Vector2 direction)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, direction);
+        if (hit.collider != null )
+        {
+            return hit.collider.gameObject;
+        } else
+        {
+            return null;
+        }
+    }
+
+    private List<GameObject> GetAllNeighbors()
+    {
+        List<GameObject> neighbors = new List<GameObject>();
+        foreach(Vector2 direction in adjacentDirections)
+        {
+            neighbors.Add(this.GetNeighbor(direction));
+        }
+        return neighbors;
+    }
+
+    private bool CanSwipe()
+    {
+        return GetAllNeighbors().Contains(previousSelected.gameObject);
+    }
+
+    private List<GameObject> FindMatch(Vector2 direction)
+    {
+        List<GameObject> matchingCandies = new List<GameObject>();
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, direction);
+        while(hit.collider != null && hit.collider.GetComponent<SpriteRenderer>().sprite == this.spriteRenderer.sprite)
+        {
+            matchingCandies.Add(hit.collider.gameObject);
+            hit = Physics2D.Raycast(hit.collider.transform.position, direction);
+        }
+        return matchingCandies;
+    }
+
+    private bool ClearMatch(Vector2[] directions)
+    {
+        List<GameObject> matchingCandies = new List<GameObject>();
+        foreach(Vector2 direction in directions)
+        {
+            matchingCandies.AddRange(FindMatch(direction));
+        }
+        if(matchingCandies.Count >= BoardManager.MinCandiesToMatch)
+        {
+            foreach(GameObject candy in matchingCandies)
+            {
+                candy.GetComponent<SpriteRenderer>().sprite = null;
+            }
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void FindallMatches()
+    {
+        if(this.spriteRenderer.sprite == null)
+        {
+            return;
+        }
+        bool hMatch = ClearMatch(new Vector2[2]
+        {
+            Vector2.left, Vector2.right
+        });
+
+        bool vMatch = ClearMatch(new Vector2[2]
+        {
+            Vector2.up, Vector2.down
+        });
+        if ( hMatch || vMatch)
+        {
+            this.spriteRenderer.sprite = null;
+            StopCoroutine(BoardManager.sharedInstance.FindNullCandies());
+            StartCoroutine(BoardManager.sharedInstance.FindNullCandies());
+        }
+    }
+
+}
---
+++ Assets/Scripts/GUIManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIManager : MonoBehaviour
+{
+    public static GUIManager sharedInstance = null;
+    public Text movesText, scoreText;
+    private int moveCounter;
+    private int score;
+
+    public int Score
+    {
+        get
+        {
+            return score;
+        }
+        set
+        {
+            score = value;
+            scoreText.text = "Score: " + score;
+        }
+    }
+
+    public int MoveCounter
+    {
+        get
+        {
+            return moveCounter;
+        }
+        set
+        {
+            moveCounter = value;
+            movesText.text = "Moves: " + moveCounter;
+            if ( moveCounter <= 0)
+            {
+                moveCounter = 0;
+                StartCoroutine(this.GameOver());
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if ( sharedInstance == null )
+        {
+            sharedInstance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        moveCounter = 5;
+        movesText.text = "Moves: " + moveCounter;
+        scoreText.text = "Score: " + score;
+    }
+
+    private IEnumerator GameOver()
+    {
+        yield return new WaitUntil(() => !BoardManager.sharedInstance.isShifting);
+        yield return new WaitForSeconds(0.025f);
+        //TODO: invocar a una pantalla diseñada para el Game Over
+    }
+
+}
